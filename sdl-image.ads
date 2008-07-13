8cdced3fefc08f042473a77c1017ba5c4c2d5aec
m0 <m0@localhost>
2008-07-13 04:02:18 +0100

    error handling and flags

--- sdl-image.ads
+++ sdl-image.ads
@@ -1,3 +1,4 @@
+with SDL.error;
 with SDL.video;
 with SDL;
 with interfaces.c.strings;
@@ -9,4 +10,9 @@ package SDL.image is
   pragma import (c, Load, "IMG_Load");
 
   function Load (file: string) return SDL.video.surface_ptr;
+
+  function GetError return cs.chars_ptr renames SDL.error.GetError;
+  function GetError return string renames SDL.error.GetError;
+  function get_error return cs.chars_ptr renames SDL.error.get_error;
+  function get_error return string renames SDL.error.get_error;
 end SDL.image;
