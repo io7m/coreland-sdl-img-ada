889b71273975515a940c99c8a52c87135d86a131
m0 <m0@localhost>
2008-07-18 01:06:31 +0100

    added missing functions.

--- sdl-image.adb
+++ sdl-image.adb
@@ -5,4 +5,24 @@ package body SDL.image is
     return Load (cs.new_string (file));
   end Load;
 
+  function LoadTyped_RW (src: SDL.rwops.rwops_ptr_t; free: boolean; typestr: string)
+    return SDL.video.surface_ptr_t is
+  begin
+    if free then
+      return LoadTyped_RW (src, 1, cs.new_string (typestr));
+    else
+      return LoadTyped_RW (src, 0, cs.new_string (typestr));
+    end if;
+  end LoadTyped_RW;
+
+  function Load_RW (src: SDL.rwops.rwops_ptr_t; free: boolean)
+    return SDL.video.surface_ptr_t is
+  begin
+    if free then
+      return Load_RW (src, 1);
+    else
+      return Load_RW (src, 0);
+    end if;
+  end Load_RW;
+
 end SDL.image;
--- sdl-image.ads
+++ sdl-image.ads
@@ -1,4 +1,5 @@
 with SDL.error;
+with SDL.rwops;
 with SDL.video;
 with SDL;
 with interfaces.c.strings;
@@ -9,7 +10,23 @@ package SDL.image is
   function Load (file: cs.chars_ptr) return SDL.video.surface_ptr_t;
   pragma import (c, Load, "IMG_Load");
 
+  function LoadTyped_RW (src: SDL.rwops.rwops_ptr_t; free: c.int; typestr: cs.chars_ptr)
+    return SDL.video.surface_ptr_t;
+  function load_typed_RW (src: SDL.rwops.rwops_ptr_t; free: c.int; typestr: cs.chars_ptr)
+    return SDL.video.surface_ptr_t renames LoadTyped_RW;
+  pragma import (c, LoadTyped_RW, "IMG_LoadTyped_RW");
+
+  function Load_RW (src: SDL.rwops.rwops_ptr_t; free: c.int)
+    return SDL.video.surface_ptr_t;
+  pragma import (c, Load_RW, "IMG_Load_RW");
+
   function Load (file: string) return SDL.video.surface_ptr_t;
+  function LoadTyped_RW (src: SDL.rwops.rwops_ptr_t; free: boolean; typestr: string)
+    return SDL.video.surface_ptr_t;
+  function load_typed_rw (src: SDL.rwops.rwops_ptr_t; free: boolean; typestr: string)
+    return SDL.video.surface_ptr_t renames LoadTyped_RW;
+  function Load_RW (src: SDL.rwops.rwops_ptr_t; free: boolean)
+    return SDL.video.surface_ptr_t;
 
   function GetError return cs.chars_ptr renames SDL.error.GetError;
   function GetError return string renames SDL.error.GetError;
