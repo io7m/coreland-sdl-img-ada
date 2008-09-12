fcc5bfecf44777c99db666396e82a1b1dbe291ef
m0 <m0@localhost>
2008-09-12 20:53:56 +0100

    insthier.c

--- insthier.c
+++ insthier.c
@@ -7,7 +7,6 @@ struct install_item insthier[] = {
   {INST_MKDIR, 0, 0, ctxt_dlibdir, 0, 0, 0755},
   {INST_MKDIR, 0, 0, ctxt_slibdir, 0, 0, 0755},
   {INST_MKDIR, 0, 0, ctxt_repos, 0, 0, 0755},
-  {INST_COPY, "sdl-ada-conf.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "sdl-img-ada-conf.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "sdl-image.ads", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "sdl-image.ads", 0, ctxt_incdir, 0, 0, 0644},
